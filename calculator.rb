af534f3100c7901a608b7f3681cc977f2ae7b742
Ivan Titov <albertduf@mail.ru>
2023-01-10 15:58:35 +0300

    first commit

---
+++ calculator.rb
@@ -0,0 +1,12 @@
+print("Введите первое число: ")
+x=gets.chomp().to_i
+print("Введите второе число: ")
+y=gets.chomp().to_i
+puts "сложение " + (x+y).to_s
+puts "вычитание " + (x-y).to_s
+puts "умножение " + (x*y).to_s
+puts "деление " + (x/y).to_s
+puts "деление с остатком " + (x/y.to_f).to_s
+puts "результат остатка от деления " + (x%y.to_f).to_s
+puts "корень " + Math.sqrt(x+y).round(3).to_s
+puts "логарифм " + Math.log(x+y).round(3).to_s
