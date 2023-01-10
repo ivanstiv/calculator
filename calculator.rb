36003c084d8f77e9672f98ba31fb7a2660ea84cc
Ivan Titov <albertduf@mail.ru>
2023-01-10 17:00:00 +0300

    calculator

--- calculator.rb
+++ calculator.rb
@@ -5,8 +5,10 @@ y=gets.chomp().to_i
 puts "сложение " + (x+y).to_s
 puts "вычитание " + (x-y).to_s
 puts "умножение " + (x*y).to_s
-puts "деление " + (x/y).to_s
+puts "деление нацело " + (x/y).to_s
 puts "деление с остатком " + (x/y.to_f).to_s
-puts "результат остатка от деления " + (x%y.to_f).to_s
+puts "результат остатка от деления " + (x%y).to_s
+puts "возведение одного числа в степень " + (x**y).to_s
+puts "модуль суммы этих чисел " + (x+y).to_s
 puts "корень " + Math.sqrt(x+y).round(3).to_s
 puts "логарифм " + Math.log(x+y).round(3).to_s
