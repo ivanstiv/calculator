33457dfacb18544bd9d523a57ee2fa68fba906c0
Ivan Titov <albertduf@mail.ru>
2023-01-14 17:06:08 +0300

    task6

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,34 @@
-print("Введите первое число: ")
-x=gets.chomp().to_i
-print("Введите второе число: ")
-y=gets.chomp().to_i
-puts "сложение " + (x+y).to_s
-puts "вычитание " + (x-y).to_s
-puts "умножение " + (x*y).to_s
-puts "деление нацело " + (x/y).to_s
-puts "деление с остатком " + (x/y.to_f).to_s
-puts "результат остатка от деления " + (x%y).to_s
-puts "возведение одного числа в степень " + (x**y).to_s
-puts "модуль суммы этих чисел " + (x+y).abs.to_s
-puts "корень " + Math.sqrt(x+y).round(3).to_s
-puts "логарифм " + Math.log(x+y).round(3).to_s
+print("Введите два числа через пробел или с операцией: ")
+input_array = gets.split
+x = input_array[0].to_i
+if input_array.size > 2
+  y = input_array[2].to_i
+  operation = input_array[1]
+  if operation == "+"
+    puts x+y
+  elsif operation == "-"
+    puts x-y
+  elsif operation == "*"
+    puts x*y
+  elsif operation == "/"
+    puts x/y
+  end
+else 
+  y = input_array[1].to_i
+  puts "сложение " + (x+y).to_s
+  puts "вычитание " + (x-y).to_s
+  puts "умножение " + (x*y).to_s
+  puts "деление с остатком " + (x/y.to_f).round(3).to_s
+  puts "результат остатка от деления " + (x%y).to_s
+  puts "возведение одного числа в степень " + (x**y).to_s
+  puts "модуль суммы этих чисел " + (x+y).abs.to_s
+  puts "корень " + Math.sqrt(x+y).round(3).to_s
+  puts "логарифм " + Math.log(x+y).round(3).to_s
+end
+
+
+
+
+
+
+>>>>>>> Stashed changes
