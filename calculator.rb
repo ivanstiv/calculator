2556a307394d5a2e519ffc3fbc0c554515bbc161
Ivan Titov <albertduf@mail.ru>
2023-01-25 17:49:32 +0300

    calc7

--- calculator.rb
+++ calculator.rb
@@ -1,13 +1,13 @@
 def calculate(x,y,operation)
-	case operation
-	when "+" 
-	    (x+y)
+  case operation
+    when "+" 
+	  x+y
     when "-" 
-    	(x-y)
+      x-y
   	when "*" 
-  		(x*y)
+  	  x*y
   	when "/" 
-  		(x/y)
+  	  x/y
   end
 end
 
@@ -29,38 +29,8 @@ x = input_array[0].to_i
 if input_array.size > 2
   y = input_array[2].to_i
   operation = input_array[1]
- puts calculate(x,y,operation)
+    puts calculate(x,y,operation)
 else
   y = input_array[1].to_i
   calculate2(x,y)
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-	
-
