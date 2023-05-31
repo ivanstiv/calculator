3d2f7d04f33f93b89d27237a76fe9dba8eb5f814
Ivan Titov <85026038+ivanstiv@users.noreply.github.com>
2023-05-31 20:59:27 +0300

    Issue 9 (#10)
    * add calculator again
    
    * fix comment
    
    * fix second comment
    
    * cons
    
    * calc4
    
    * calc4

--- calc4.rb
+++ calc4.rb
@@ -11,6 +11,24 @@ def calculate2(x,y,sign)
     end
 end
 
+def calculate3(x,y)
+  operations = {
+    "+" => ['Сложение: ', x+y],
+    "-" => ['Вычитание: ', x-y],
+    "*" => ['Умножение: ', x*y],
+    "/" => ['Деление нацело: ', x/y],
+    "&" => ['Остаток от деления: ', x/y],
+    "^" => ['Возведение в степень: ', x**y],
+    "%" => ['Остаток от процента: ', x*y/100],
+    "++" => ['Модуль суммы этих чисел: ', (x+y).abs],
+    "//" => ['Корень: ', Math.sqrt(x+y).round(3)],
+    "#" =>  ['Логарифм: ', Math.log(x+y).round(3)]
+  }
+  operations.each do |k,v|
+    puts v[0]+v[1].to_s
+  end
+end
+
 print("Введите два числа через пробел: ")
 array = gets.split
 x = array[0].to_i
@@ -20,18 +38,6 @@ if array.size > 2
   puts calculate2(x,y,sign)
 else
   y = array[1].to_i
-  operations = {
-    "+" => ['Сложение: ', x+y],
-    "-" => ['Вычитание: ', x-y],
-    "*" => ['Умножение: ', x*y],
-    "/" => ['Деление нацело: ', x/y],
-    "%" => ['Остаток от деления: ', x/y],
-    "**" => ['Возведение в степень: ', x**y],
-    "++" => ['Модуль суммы этих чисел: ', (x+y).abs],
-    "//" => ['Корень: ', Math.sqrt(x+y).round(3)],
-    "#" =>  ['Логарифм: ', Math.log(x+y).round(3)]
-  }
-  operations.each do |k,v|
-    puts v[0]+v[1].to_s
-  end
+  calculate3(x,y)
 end
+   
