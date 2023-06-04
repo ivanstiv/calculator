dda701c0a6592ddbc4cdd84d3573eedb8dbe457f
Ivan Titov <85026038+ivanstiv@users.noreply.github.com>
2023-06-04 10:40:20 +0300

    Issue 10 (#13)
    * add calculator again
    
    * fix comment
    
    * fix second comment
    
    * cons
    
    * calc4
    
    * calc4
    
    * num
    
    * arab

--- roman_to_integer.rb
+++ roman_to_integer.rb
@@ -1,45 +1,18 @@
-def roman_to_integer(roman)
+ROMAN_TO_INT =
+{
+  i: 1,
+  v: 5,
+  x: 10,
+  l: 50,
+  c: 100,
+  d: 500,
+  m: 1000
+}
 
-  num_I = roman.count("I") 
-  num_V = roman.count("V")
-  num_X = roman.count("X")
-  num_L = roman.count("L")
-  num_C = roman.count("C")
-  num_D = roman.count("D")
-  num_M = roman.count("M")
-
-  rom_num = { "I" => 1, 
-              "V" => 5, 
-              "X" => 10,
-              "L" => 50,
-              "C" => 100,
-              "D" => 500,
-              "M" => 1000 }
-
-  if roman[0] == "M"
-    integer  = num_M * rom_num["M"] + num_D * rom_num["D"] + num_C * rom_num["C"] + num_L * rom_num["L"] + num_X * rom_num["X"] + num_V * rom_num["V"] + num_I * rom_num["I"]
-  elsif roman[0] == "D"
-    integer = num_D * rom_num["D"] + num_C * rom_num["C"] + num_L * rom_num["L"] + num_X * rom_num["X"] + num_V * rom_num["V"] + num_I * rom_num["I"]
-
-  elsif roman[0] == "C"
-    integer = num_C * rom_num["C"] + num_L * rom_num["L"] + num_X * rom_num["X"] + num_V * rom_num["V"] + num_I * rom_num["I"]
-
-  elsif roman[0] == "L"
-    integer = num_L * rom_num["L"] + num_X * rom_num["X"] + num_V * rom_num["V"] + num_I * rom_num["I"]
-
-  elsif roman[0] == "X"
-    integer = num_X * rom_num["X"] + num_V * rom_num["V"] + num_I * rom_num["I"]
-
-  elsif roman[0] == "V"
-    integer = num_V * rom_num["V"] + num_I * rom_num["I"]
-
-  elsif roman[0] == "I"
-    integer = num_I * rom_num["I"]
-  end
-
-  return integer
+def roman_to_int(roman)
+  numbers = roman.downcase.chars.map { |char| ROMAN_TO_INT[char.to_sym] }.reverse
+  numbers.inject([0, 1]) do |result_number, int|
+    result, number = result_number
+    int >= number ? [result + int, int] : [result - int, number]
+  end.first
 end
-
-print("Введите римское число: ")
-number = gets.chomp
-puts roman_to_integer(number)
