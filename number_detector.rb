ad8c9ddd9483cd1177bf0207bfe70a5afb9fd24a
ivanstiv <albertduf@mail.ru>
2023-06-18 20:27:55 +0300

    calcul

--- number_detector.rb
+++ number_detector.rb
@@ -1,9 +1,9 @@
 def detect_number(symbol)
   detect_number = :Roman 
     if symbol.match?(/\A[IVXCLDM]*\z/)
-  	 return detect_number
+    return detect_number
 elsif symbol.match?(/\A\d*\z/)
   detect_number = :Integer
-  	 return detect_number
+    return detect_number
 end
 end
