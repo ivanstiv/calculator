9e80368d70a1516d77dc74fb9ba5d8f80fa76c7b
ivanstiv <albertduf@mail.ru>
2023-06-21 21:25:22 +0300

    cals

--- number_detector.rb
+++ number_detector.rb
@@ -1,9 +1,7 @@
 def detect_number(symbol)
-  detect_number = :roman 
-if symbol.match?(/\A[IVXCLDM]*\z/)
-  return detect_number
-elsif symbol.match?(/\A\d*\z/)
-  detect_number = :integer
-  return detect_number
+    if symbol.match?(/\A[IVXCLDM]*\z/)
+       :Roman
+    elsif symbol.match?(/\A\d*\z/)
+       :Integer
 end
 end
